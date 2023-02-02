057988e1301bc0746ecbff64b5a72ab0cc0075c6
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-02 19:12:50 -0300

    criado lista de opções do menu na barra lateral

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-import "./SideBar.scss"
+import "./SideBar.scss";
 
 export default function SideBar() {
-  return <div className="side-bar">SideBar</div>;
+  return (
+    <div className="side-bar">
+      <ul className="side-bar__menu">
+        <li className="side-bar__menu__item">Sobre</li>
+        <li className="side-bar__menu__item">Projetos</li>
+        <li className="side-bar__menu__item">Contato</li>
+      </ul>
+    </div>
+  );
 }

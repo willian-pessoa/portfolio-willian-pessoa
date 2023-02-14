67bfde0222f98c04ef44116c07f4420361049565
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-14 14:59:09 -0300

    componente projectcard modificado, adicionado um novo projeto para a seção

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -6,11 +6,19 @@ const PROJECTS = [{
   demo: "https://willian-pessoa.github.io/blizzard-challenge/"
 },
 {
+  name: "Comms Reviewer",
+  description: "Web App para revisar comutadores de BLD, são parzinhos de letras associados com algoritmos, o app mostra esses pares de forma aleatoria de acordo com o algortimo do app.",
+  image: "/assets/comms-reviewer.jpg",
+  github: "https://github.com/willian-pessoa/ultimate-bld-trainer",
+  demo: "https://comms-reviewer.netlify.app/",
+},
+{
   name: "Portfolio",
   description: "Portfolio pessoal com intuito de praticar habilidades de front end e construir um espaço para apresentar os principais projetos até o momento.",
   image: "/assets/portfolio-sobre.jpg",
   github: "https://github.com/willian-pessoa/portfolio-willian-pessoa",
   demo: "https://github.com/willian-pessoa/portfolio-willian-pessoa"
-}]
+},
+]
 
 export default PROJECTS

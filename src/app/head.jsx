ef90857f1880cba9ea50508d654d7d53fd97aec9
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-14 20:49:40 -0300

    atualização meta tags

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -3,14 +3,16 @@ export default function Head() {
     <>
       <title>Willian Pessoa | Portfolio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="author" content="Willian Pessoa"/>
       <meta
         name="description"
-        content="Portfolio pessoal de Willian Pessoa, desenvolvedor front-end."
+        content="Portfolio pessoal de Willian Pessoa desenvolvedor front-end."
       />
       <meta
         name="keywords"
-        content="front-end, react js, next js, desenvolvedor front end, desenvolvedor react, portfolio, willian pessoa"
+        content="front-end, react js, next js, desenvolvedor front-end, desenvolvedor react, portfolio, willian pessoa"
       />
+      <meta name="google-site-verification" content="lALi_FaP-gqd452oXO5dNr97av5ExCKUNdqUQA1EC5Q" />
     </>
   );
 }

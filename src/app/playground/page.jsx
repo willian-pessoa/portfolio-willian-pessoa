6e7a0f24afb07b3f718bb5b19687916a0cc8ea3b
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-13 22:14:23 -0300

    criado pagina de teste para construção componente de carousel

---
+++ src/app/playground/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Carousel from "@/components/sharedComponents/Carousel/Carousel";
+
+const Playground = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
+
+export default Playground;
---
+++ src/components/sharedComponents/Carousel/Carousel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./Carousel.scss"
+
+const Carousel = () => {
+  return (
+    <div className='container-pai'>
+      <div className="carousel">
+
+      </div>
+    </div>
+  )
+}
+
+export default Carousel
